d48ab825fbf64ef2332aaa841aef4aaaa7fc1dba
Ryan Carniato <ryansolid@gmail.com>
2021-06-27 22:59:50 -0700

    bump to solid 1.0

--- test/server.test.jsx
+++ test/server.test.jsx
@@ -3,6 +3,6 @@ import { renderToString } from "solid-js/web"
 describe("Test the server", () => {
   it("should render in the server", () => {
     const d = renderToString(() => <div title="Welcome to Jest">text content</div>);
-    expect(d.html).toBe(`<div data-hk="0" title="Welcome to Jest">text content</div>`);
+    expect(d).toBe(`<div data-hk="0" title="Welcome to Jest">text content</div>`);
   })
 })
